b7bc283aa4daeaa47200edc0b2c9be0c5f8893cd
gujial <gu18647403665@outlook.com>
2025-10-29 08:15:16 +0800

    修复缩进问题

--- flake.nix
+++ flake.nix
@@ -59,28 +59,28 @@
           mkdir -p $out/share/applications $out/share/icons/hicolor/128x128/apps
           cp $out/opt/tmm/tmm.png $out/share/icons/hicolor/128x128/apps/tinymediamanager.png
           cat > $out/share/applications/tinymediamanager.desktop << DESKTOP
-            [Desktop Entry]
-            Name=tinyMediaManager
-            GenericName=Media Manager
-            Comment=Manage and organize your media collection
-            Exec=$out/bin/tinyMediaManager %u
-            Terminal=false
-            Type=Application
-            Categories=AudioVideo;Video;Utility;
-            Icon=tinymediamanager
-            DESKTOP
+[Desktop Entry]
+Name=tinyMediaManager
+GenericName=Media Manager
+Comment=Manage and organize your media collection
+Exec=$out/bin/tinyMediaManager %u
+Terminal=false
+Type=Application
+Categories=AudioVideo;Video;Utility;
+Icon=tinymediamanager
+DESKTOP
           chmod 644 $out/share/applications/tinymediamanager.desktop
           '';
 
           installPhase = ''
           mkdir -p $out/bin
           cat > $out/bin/tinyMediaManager << EOF
-            #!/bin/sh
-            export LD_LIBRARY_PATH=${pkgs.libzen}/lib:${pkgs.libmediainfo}/lib:\$LD_LIBRARY_PATH
-            export PATH=${pkgs.zenity}/bin:\$PATH
-            cd $out/opt/tmm
-            exec ${pkgs.zulu23}/bin/java -Djava.library.path=./native -cp "tmm.jar:lib/*" org.tinymediamanager.TinyMediaManager "\$@"
-            EOF
+#!/bin/sh
+export LD_LIBRARY_PATH=${pkgs.libzen}/lib:${pkgs.libmediainfo}/lib:\$LD_LIBRARY_PATH
+export PATH=${pkgs.zenity}/bin:\$PATH
+cd $out/opt/tmm
+exec ${pkgs.zulu23}/bin/java -Djava.library.path=./native -cp "tmm.jar:lib/*" org.tinymediamanager.TinyMediaManager "\$@"
+EOF
           chmod +x $out/bin/tinyMediaManager
           '';
         };
